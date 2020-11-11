0df420508a1f61d1c53b826736b4b487877ddaae
rakin <rakin92@gmail.com>
2020-11-11 09:42:41 -0500

    init commit

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import { useRouter } from 'next/router'
+
+import USAMap from "react-usa-map";
+import Layout from '../components/Layout/Layout';
+
+export default function Home() {
+  const router = useRouter()
+  const mapHandler = (event: any) => {
+    router.push(`/usa/${event.target.dataset.name}`);
+  };
+
+  return (
+    <Layout> 
+      <USAMap onClick={mapHandler} defaultFill={"#000000"} title={"United States of America"}/>
+      <style global jsx>{`
+        path {
+          pointer-events: all;
+        }
+        path:hover {
+            opacity: 0.5;
+            cursor: pointer;
+        }
+      `}</style>
+    </Layout>
+  )
+}
